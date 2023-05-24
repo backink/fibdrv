cca1648e530c7666a00692f327715a9116b258c8
Lin Chun Yeh <r09921093@g.ntu.edu.tw>
2023-05-24 14:06:14 +0800

    Add python script for creating data graphs

--- time_analysis.sh
+++ time_analysis.sh
@@ -4,7 +4,7 @@
 # first arg is the fib function number, which 0 is dp and 1 is fast doubling.
 # second arg is the offset
 cd fd_data
-for ((i = 0; i <= 1000; i+=10))
+for ((i = 0; i <= 1000; i+=2))
 do
     filename="${i}.txt"
     touch "$filename"
