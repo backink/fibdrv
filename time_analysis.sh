e34f15400d471b48bc0628bace0ad0b4c264addf
Lin Chun Yeh <r09921093@g.ntu.edu.tw>
2023-06-07 14:36:02 +0800

    Isolate CPU to analyze execution time

--- time_analysis.sh
+++ time_analysis.sh
@@ -1,14 +1,24 @@
-# !/bin/sh
+#!/bin/bash
 
 # script for recording kernel execution time
 # first arg is the fib function number, which 0 is dp and 1 is fast doubling.
 # second arg is the offset
 cd fd_data
-for ((i = 0; i <= 1000; i+=2))
-do
-    filename="${i}.txt"
-    touch "$filename"
 
-    sudo ~/linux2023/fibdrv/client 1 "$i" >> "$filename"
+for ((i=0; i<=1000; i+=2))
+do
+	filename="${i}.txt"
+	touch "$filename"
+
+	sudo taskset -c 0,1 ../client 1 "$i" >> "$filename"
 done
 
+cd ../dp_data
+
+for((i=0; i<=1000; i+=2))
+do
+	filename="${i}.txt"
+	touch "$filename"
+	
+	sudo taskset -c 0,1 ../client 0 "$i" >> "$filename"
+done
